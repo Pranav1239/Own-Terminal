1d60b2d753960a0989f84b4b12efac922180a2d8
Pranav1239 <pranavkumar192192@gmail.com>
2024-07-10 06:45:49 +0530

    files added

--- internal/ssh/ssh.go
+++ internal/ssh/ssh.go
@@ -1,1 +1,19 @@
 package ssh
+
+// "fmt"
+// "os/exec"
+
+// SigninToSSH signs in to an SSH server with the provided host and user.
+// func SigninToSSH(host, user string) error {
+// cmd := exec.Command("ssh", fmt.Sprintf("%s@%s", user, host))
+// cmd.Stdout = os.Stdout
+// cmd.Stdin = os.Stdin
+// cmd.Stderr = os.Stderr
+
+// err := cmd.Run()
+// if err != nil {
+// 	return fmt.Errorf("error signing in to SSH: %v", err)
+// }
+
+// return nil
+// }
--- internal/todo/todo.go
+++ internal/todo/todo.go
@@ -1,1 +1,40 @@
+// Package todo provides functions for managing to-do items.
 package todo
+
+// import (
+// 	"encoding/json"
+// 	"fmt"
+// 	"io/ioutil"
+// 	"os"
+// )
+
+// // Task struct represents a single to-do item.
+// type Task struct {
+// 	ID   string `json:"id"`
+// 	Text string `json:"text"`
+// }
+
+// var todosFile = "data/todos.json"
+
+// // AddTodo adds a new to-do item with the given text.
+// func AddTodo(text string) error {
+// 	todos, err := readTodos()
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	task := Task{
+// 		ID:   fmt.Sprintf("%d", len(todos)+1),
+// 		Text: text,
+// 	}
+// 	todos = append(todos, task)
+
+// 	err = saveTodos(todos)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	return nil
+// }
+
+// // ViewTodos retrieves and returns all stored to-do items
